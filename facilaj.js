faf71a6eb2d0f7b3dd3c870d5dd9b0bc1d7a5ac0
Magnus Henoch <magnus.henoch@gmail.com>
2013-04-02 02:44:30 +0100

    Aldonis klarigon pri "tipoj" en vortolistoj

--- facilaj.js
+++ facilaj.js
@@ -1,4 +1,9 @@
 // Kopiita el http://remush.be/tezauro/Kontakto.html
+//
+// Ĉiu vorto en la listo havas "tipon", aŭ 1 aŭ 2.
+//
+// 1: La vorto estas radiko plus vortoklasa finaĵo (-o, -a, -i, -e).
+// 2: La vorto estas "vorteto", uzebla sen finaĵo.
 var vortaroFacilaj = {
   "aboni": 1,
   "afabla": 1,
--- trefacilaj.js
+++ trefacilaj.js
@@ -1,4 +1,12 @@
 // Kopiita el http://remush.be/tezauro/Kontakto.html
+//
+// Ĉiu vorto en la listo havas "tipon", aŭ 1 aŭ 2.
+//
+// 1: La vorto estas radiko plus vortoklasa finaĵo (-o, -a, -i, -e).
+// 2: La vorto estas "vorteto", uzebla sen finaĵo.
+//
+// Prefiksoj kaj sufiksoj povus esti en la ĉefa listo laŭ tipo 1, sed
+// por konveno ili estas listigitaj en apartaj listoj sube.
 var vortaroTreFacilaj = { /* "-a", */
   /* "-aĉ", */
   "aĉeti": 1,
